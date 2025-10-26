194d1897332d2992cc82ebe96d4e3c27f86cd7e9
Pardeep Kumar <162547409+lazyhacker24@users.noreply.github.com>
2025-10-26 11:48:29 +0530

    app.py

--- my-project/backend/app.py
+++ my-project/backend/app.py
@@ -1,1 +1,14 @@
+from flask import Flask, jsonify, request
+app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return jsonify({"message": "Hello from Flask Backend!"})
+
+@app.route('/data', methods=['POST'])
+def data():
+    data = request.json
+    return jsonify({"received": data})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
